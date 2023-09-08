7d422adea481179cf72c0454c7fbe04d97ca7c6b
EstebanMqz <emarquez1895@gmail.com>
2023-09-08 17:18:44 -0600

    grep regex & web cat piped elements

--- tag-search.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Description: This script will search for a tag in the README.md file and replace it with a web-search.
-
-grep -oP '(?<=##\s\d\.\s)[\w\s]+' README.md > test.txt && code --reuse-window "test.txt" #1-9 only extraction.
